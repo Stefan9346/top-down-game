2204f1b70504d0fc078918e471be9693226d3c0f
Stefan <sos_stef@hotmail.com>
2018-04-23 11:21:37 +0200

    Player Movement Script Added

---
+++ top-down-project/top-down-group-project/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+        }
+    }
+}
