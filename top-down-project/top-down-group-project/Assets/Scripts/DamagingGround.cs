4b720ec0c8414245e09dffa2198acecb98002a9f
Stefan <sos_stef@hotmail.com>
2018-04-25 16:34:41 +0200

    damage over time update

--- top-down-project/top-down-group-project/Assets/Scripts/DamagingGround.cs
+++ top-down-project/top-down-group-project/Assets/Scripts/DamagingGround.cs
@@ -6,27 +6,51 @@ public class DamagingGround : MonoBehaviour
 {
 
 
+    public float damageOverTime;
 
 
 
     void Start()
     {
-
+       
     }
 
 
     void Update()
     {
-
+       
     }
 
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.transform.tag == "Player")
-        {
-            collision.gameObject.GetComponent<Health>().health -= 1;
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
 
+    //    if (damageOverTime > 0f)
+    //    {
+    //        damageOverTime -= Time.deltaTime;
+    //    }
+
+    //    else if (collision.transform.tag == "Player")
+    //    {
+    //        collision.gameObject.GetComponent<Health>().health -= 1;
+    //        damageOverTime = 0.5f;
+
+    //    }
+
+
+    //}
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            damageOverTime -= Time.deltaTime;
+            if (damageOverTime <= 0)
+            {
+                collision.gameObject.GetComponent<Health>().health -= 1;
+                damageOverTime = 1f;
+            }
         }
     }
 
