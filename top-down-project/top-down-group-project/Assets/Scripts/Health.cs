d889eb04052dbef534666466f6f7c87e88f26658
Stefan <sos_stef@hotmail.com>
2018-04-25 13:17:17 +0200

    added health / hearts script

---
+++ top-down-project/top-down-group-project/Assets/Scripts/Health.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+    public int health;
+    public int numOfHearts;
+
+    public Image[] hearts;
+    public Sprite heartFull;
+    public Sprite heartEmpty;
+
+    void Start () {
+		
+	}
+	
+	
+	void Update ()
+    {
+
+        if(health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
+
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if(i < health)
+            {
+                hearts[i].sprite = heartFull;
+            }
+            else
+            {
+                hearts[i].sprite = heartEmpty;
+            }
+
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    
+	}
+}
