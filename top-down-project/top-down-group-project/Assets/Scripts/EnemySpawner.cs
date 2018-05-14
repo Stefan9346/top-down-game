f95921aa4586024ec85e28692d7e4c5b70eaa473
Stefan <sos_stef@hotmail.com>
2018-05-14 20:51:27 +0200

    enemySpawner
    Worked on enemy spawner script, would say about 60% done

---
+++ top-down-project/top-down-group-project/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public enum SpawnState { SPAWNING, WAITING, COUNTING};
+
+
+	[System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+
+    private SpawnState state = SpawnState.COUNTING;
+
+
+
+	void Start ()
+    {
+        waveCountdown = timeBetweenWaves;
+	}
+	
+
+
+
+	void Update ()
+    {
+		if(waveCountdown <= 0)
+        {
+            if(state != SpawnState.SPAWNING)
+            {
+                StartCoroutine( SpawnWave ( waves[nextWave] ) );
+            }
+        }
+
+        else
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+	}
+
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        state = SpawnState.SPAWNING;
+
+        for (int i = 0; i <_wave.count; i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(1f / _wave.rate);
+        }
+
+        state = SpawnState.WAITING;
+
+
+        yield break;
+    }
+
+    void SpawnEnemy(Transform _enemy)
+    {
+        // Spawn Enemy
+        Debug.Log("Spawning Enemy: " + _enemy.name);
+    }
+
+}
+
